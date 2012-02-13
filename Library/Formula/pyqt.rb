82b071d5d4b3579686cad353d23dfb25c77fdf3d
Barrett Clark <bclark@geoforce.com>
2012-02-12 22:16:27 -0600

    Resolved my merge conflict with the pyqt recipe

--- Library/Formula/pyqt.rb
+++ Library/Formula/pyqt.rb
@@ -6,7 +6,7 @@ require 'formula'
 class Pyqt < Formula
   url 'http://www.riverbankcomputing.co.uk/static/Downloads/PyQt4/PyQt-mac-gpl-4.9.1.tar.gz'
   homepage 'http://www.riverbankcomputing.co.uk/software/pyqt'
-  md5 '3b9112a49a3484032f29ea7fe65c31e5'
+  sha1 '6c0dbf0edb9a0f07fb3ed95f6c3b4b5d0458dbe7'
 
   depends_on 'sip'
   depends_on 'qt'
